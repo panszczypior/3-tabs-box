b99491ef4b7986fe69d14465d72ae8ff1f9f481c
Przemek <lprzemek@gmail.com>
2017-03-30 19:50:46 +0200

    flickr api

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const httpGet = (url) => {
+    const xmlHttp = new XMLHttpRequest();
+    const promise = new Promise((resolve, reject) => {
+      xmlHttp.onreadystatechange = function() {
+          if (xmlHttp.readyState === 4 && xmlHttp.status == 200)
+              resolve(xmlHttp.responseText);
+          } else {
+            reject();
+            // add reject to other files, throw error or smt
+          }
+    });
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(null);
+    return promise;
+};
+
+httpGet('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=91bace887faabf57a704cea71a25eaf2&format=json&nojsoncallback=1&text=Krak%C3%93w&min_taken_date=2017-01-01&sort=date-posted-desc')
+  .then(data => {
+  console.log(data);
+});
+
+
+// add eslinter
+//construct link to picture, note that size matters
