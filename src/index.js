4162431f41ddc2f67c8c9a03ab24b928109c4410
Przemek <lprzemek@gmail.com>
2017-03-31 16:07:07 +0200

    eslint installed

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 const httpGet = (url) => {
-    const xmlHttp = new XMLHttpRequest();
-    const promise = new Promise((resolve, reject) => {
-      xmlHttp.onreadystatechange = () => {
-          if(xmlHttp.readyState === 4 && xmlHttp.status == 200){
-            resolve(JSON.parse(xmlHttp.responseText));
-          }
+  const xmlHttp = new XMLHttpRequest();
+  const promise = new Promise((resolve, reject) => {
+    xmlHttp.onreadystatechange = () => {
+      if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+        resolve(JSON.parse(xmlHttp.responseText));
       }
-    });
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send(null);
-    return promise;
+    };
+  });
+
+  xmlHttp.open('GET', url, true);
+  xmlHttp.send(null);
+  return promise;
 };
 
 httpGet('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=91bace887faabf57a704cea71a25eaf2&format=json&nojsoncallback=1&text=Krak%C3%93w&min_taken_date=2017-01-01&sort=date-posted-desc')
-  .then(data => {
+  .then((data) => {
   console.log(data);
-});
+  });
 
 
-// add eslinter
-//construct link to picture, note that size matters
-// reject
+//  add eslinter
+//  construct link to picture, note that size matters
+//  reject
--- src/logs.js
+++ src/logs.js
@@ -5,34 +5,34 @@ const readTextFile = (file) => {
   const promise = new Promise((resolve, reject) => {
     rawFile.open('GET', file, false);
     rawFile.onreadystatechange = () => {
-      if(rawFile.readyState === 4 && (rawFile.status === 200 || rawFile.status == 0)){
+      if (rawFile.readyState === 4 && (rawFile.status === 200 || rawFile.status === 0)) {
         resolve(rawFile.responseText.split(/\n/).map(parse).map(item => item.path));
       }
-    }
+    };
     rawFile.send(null);
   });
 
   return promise;
-}
+};
 
 const parsedLog = readTextFile('../assets/varnish.log')
-  .then(data => {
-  const paths = data.reduce((acc, currVal) => {
+  .then((data) => {
+    const paths = data.reduce((acc, currVal) => {
       const matches = currVal ? currVal.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i) : [];
       const hostname = matches[1];
-      acc.hostnames[hostname] = acc.hostnames[hostname] ? acc.hostnames[hostname]+1 : 1
-      acc.files[currVal] = acc.files[currVal] ? acc.files[currVal]+1 : 1;
+      acc.hostnames[hostname] = acc.hostnames[hostname] ? acc.hostnames[hostname] + 1 : 1
+      acc.files[currVal] = acc.files[currVal] ? acc.files[currVal] + 1 : 1;
       return acc;
-    },{files:{}, hostnames:{}});
+    }, { files: {}, hostnames: {} });
     return paths;
   })
-  .then(paths => {
-    const {hostnames, files} = paths;
-    const hostnamesSorted = Object.keys(hostnames).sort((a,b) => hostnames[b]-hostnames[a]);
-    const filesSorted = Object.keys(files).sort((a,b) =>  files[b]-files[a]);
+  .then((paths) => {
+    const { hostnames, files } = paths;
+    const hostnamesSorted = Object.keys(hostnames).sort((a, b) => hostnames[b] - hostnames[a]);
+    const filesSorted = Object.keys(files).sort((a, b) => files[b] - files[a]);
   });
 
 
-//add localstorage
-//add alternative to get hostnames a.href
+// add localstorage
+// add alternative to get hostnames a.href
 // check rawFile.status === 0
