6832cb3b9f99c7d7bda2c04f63df913a91efafea
Przemek <lprzemek@gmail.com>
2017-04-02 19:42:08 +0200

    refactored feeds

--- src/feeds.js
+++ src/feeds.js
@@ -2,41 +2,33 @@ import config from './config.json';
 
 const feeds = [];
 
-feednami.load(config.vgUrl) /* eslint no-undef: 0 */
+const populateFeeds = (feed) => {
+  for (const entry of feed) {
+    const { title, date_ms } = entry;
+    feeds.push({ title, date_ms });
+  }
+};
+
+const sortFeeds = feed => (
+  feed
+    .sort((first, second) => second.date_ms - first.date_ms)
+    .map(item => ({ ...item, isoDate: new Date(item.date_ms) }))
+);
+
+export default feednami.load(config.vgUrl) /* eslint no-undef: 0 */
   .then((feed) => {
-    for (const entry of feed.entries) {
-      const { title, date_ms } = entry;
-      feeds.push({ title, date_ms });
-    }
+    populateFeeds(feed.entries);
     return feednami.load(config.registerUrl);
   })
-  .catch(err => {
-    console.log(err);
-    return feednami.load(config.registerUrl);
-  })
+  .catch(() => feednami.load(config.registerUrl))
   .then((feed) => {
-    for (const entry of feed.entries) {
-      const { title, date_ms } = entry;
-      feeds.push({ title, date_ms });
-    }
+    populateFeeds(feed.entries);
     return feednami.load(config.arstechnica);
   })
-  .catch(err => {
-    console.log(err);
-    return feednami.load(config.arstechnica);
-  })
+  .catch(() => feednami.load(config.arstechnica))
   .then((feed) => {
-    for (const entry of feed.entries) {
-      const { title, date_ms } = entry;
-      feeds.push({ title, date_ms });
-    }
-    const arr = feeds.sort((a, b) => b.date_ms - a.date_ms).map(item => {
-      const isoDate = new Date(item.date_ms);
-      return {
-        ...item,
-        isoDate,
-      };
-    });
-    console.log(arr);
+    populateFeeds(feed.entries);
+    const arr = sortFeeds(feeds);
+    return arr;
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
