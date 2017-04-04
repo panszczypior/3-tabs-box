5c3b9a94337ab545d0f87acbbabd46c7cf74d3d8
Przemek <lprzemek@gmail.com>
2017-04-04 22:46:57 +0200

    render module

---
+++ src/render.js
@@ -0,0 +1,87 @@
+/* eslint no-underscore-dangle:0 */
+import logs from './logs';
+import feeds from './feeds';
+import flickrApi from './flickr';
+import { styles, tabContentIds, classNames } from './consts';
+import { createList, createGallery } from './helpers';
+
+const renderStaticContent = (container, content) => {
+  const _container = container;
+  _container.style.display = styles.block;
+  _container.appendChild(content);
+};
+
+const renderDynamicContent = (container, content) => {
+  const _container = container;
+  _container.style.display = styles.block;
+  _container.innerHTML = '';
+  _container.appendChild(content);
+};
+
+const clearPrevTab = (containerId) => {
+  const elem = document.getElementById(containerId);
+  if (elem) {
+    elem.style.display = 'none';
+  }
+};
+
+const render = {
+  renderStaticContent,
+  renderDynamicContent,
+  clearPrevTab,
+};
+
+const renderLogs = () => {
+  const containerId = tabContentIds.logs;
+  const container = document.getElementById(containerId);
+  logs.get().then(({ hostnames, files }) => {
+    const topHostnames = hostnames.slice(0, 5);
+    const topFiles = files.slice(0, 5);
+    const { list, listItem } = classNames;
+    const hostsList = createList({
+      list,
+      item: listItem,
+    }, topHostnames, tabContentIds.logs);
+
+    const filesList = createList({
+      list,
+      item: listItem,
+    }, topFiles, tabContentIds.logs);
+    container.innerHTML = '';
+    renderStaticContent(container, hostsList);
+    renderStaticContent(container, filesList);
+  });
+};
+
+const renderFeeds = () => {
+  const containerId = tabContentIds.feeds;
+  const container = document.getElementById(containerId);
+  feeds.get().then((data) => {
+    const { list, listItem } = classNames;
+    const feed = createList({
+      list,
+      item: listItem,
+    }, data, tabContentIds.feeds);
+    renderDynamicContent(container, feed);
+  });
+};
+
+const renderFlickr = (text) => {
+  const containerId = tabContentIds.flickr;
+  const container = document.getElementById(containerId);
+  flickrApi.get(text).then((data) => {
+    const flickr = createGallery({
+      gallery: classNames.photosGallery,
+      photo: classNames.photo,
+    }, data);
+    renderStaticContent(container, flickr);
+  });
+};
+
+export {
+  render as default,
+  clearPrevTab,
+  renderLogs,
+  renderFeeds,
+  renderFlickr,
+};
